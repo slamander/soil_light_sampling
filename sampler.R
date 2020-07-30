18bd554a34dcde99922e235dc718d337acca965a
Alex Baecher <jbaecher@ufl.edu>
2020-07-30 09:20:29 -0400

    Updated code, full push

---
+++ sampler.R
@@ -0,0 +1,11 @@
+source("https://raw.githubusercontent.com/slamander/soil_light_sampling/master/source.R")
+
+# soil/light sampler
+
+# to generate random numbers for soil sampling:
+sampling()    # soil is the default..
+# you can also use:
+sampling("soil")
+
+# to generate random numbers for light sampling:
+sampling("light")
